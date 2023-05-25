63a200d294d4baf87ee180882d67245e5bdf9911
Héctor Roberto Nuricumbo Castro <hector@MacBook-Air-de-Hector.local>
2023-05-25 11:02:14 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './componentes/Login';
+import { Row,Container,Col } from 'react-bootstrap';
+import Menunav from './componentes/Menunav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className='App'>
+        <Menunav></Menunav>
+      </div>
+      <Row>
+        <Col></Col>
+        <Col>
+          <div className="App">
+            <Login></Login>
+          </div>
+        </Col>
+        <Col></Col>
+        </Row>
+    </Container>
   );
 }
 
 export default App;
+
+//<img src={logo} className="App-logo" alt="logo" />
